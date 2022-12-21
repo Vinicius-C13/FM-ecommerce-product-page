5f897438bf5269f845421bcc60fd3fb8ef90ea14
Vinícius Costa <96350207+Vinicius-C13@users.noreply.github.com>
2022-12-20 21:26:16 -0300

    Mobile version done. Carousel done.

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ function toggleMenu(event) {
     const nav = document.querySelector('.nav');
     nav.classList.contains('show-nav') ? nav.classList.remove('show-nav') : nav.classList.add('show-nav');
 }
+
+const carousel = (() => {
+
+    let currentSlideIndex = 1;
+    const slidesNumber = document.querySelector('.images-container').children.length;
+
+    function nextSlide() {
+        const slideWidth = Number(document.querySelector('.carousel-item').offsetWidth);
+        const carousel = document.querySelector('.images-container');
+
+        if(currentSlideIndex < slidesNumber) {
+            carousel.style.left = `${-(slideWidth * currentSlideIndex)}px`;
+            currentSlideIndex++;
+        }
+    }
+
+    function prevSlide() {
+        const slideWidth = Number(document.querySelector('.carousel-item').offsetWidth);
+        const carousel = document.querySelector('.images-container');
+
+        if(currentSlideIndex > 1) {
+            carousel.style.left = `${-(slideWidth * (currentSlideIndex - 1)) + slideWidth}px`;
+            currentSlideIndex-- ;
+        }
+    }
+    return {nextSlide, prevSlide}
+})()
