fed57844bcb42fe1697775a7a4215cecc17ef7d0
Vinícius Costa <96350207+Vinicius-C13@users.noreply.github.com>
2022-12-21 15:30:17 -0300

    Carousel finished

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ function toggleMenu(event) {
 const carousel = (() => {
 
     let currentSlideIndex = 1;
-    const slidesNumber = document.querySelector('.images-container').children.length;
     let slidesGap = 10;
+    let slidePosition = 0;
+    const slidesNumber = document.querySelector('.images-container').children.length;
 
     function nextSlide() {
         const slideWidth = Number(document.querySelector('.carousel-item').offsetWidth) + slidesGap;
@@ -15,7 +16,9 @@ const carousel = (() => {
 
         if(currentSlideIndex < slidesNumber) {
             carousel.style.left = `${-(slideWidth * currentSlideIndex)}px`;
+            slidePosition = -(slideWidth * currentSlideIndex);
             currentSlideIndex++;
+            highlightCurrentSlide(currentSlideIndex-1);
         }
     }
 
@@ -25,8 +28,29 @@ const carousel = (() => {
 
         if(currentSlideIndex > 1) {
             carousel.style.left = `${-(slideWidth * (currentSlideIndex - 1)) + slideWidth}px`;
+            slidePosition = -(slideWidth * (currentSlideIndex - 1)) + slideWidth;
             currentSlideIndex-- ;
+            highlightCurrentSlide(currentSlideIndex-1);
         }
     }
-    return {nextSlide, prevSlide}
+
+    function selectSlide(index) {
+        const slideWidth = Number(document.querySelector('.carousel-item').offsetWidth) + slidesGap;
+        const carousel = document.querySelector('.images-container');
+        const difference = currentSlideIndex - index;
+
+        carousel.style.left = `${slidePosition + (slideWidth * difference)}px`;
+        slidePosition = slidePosition + (slideWidth * difference);
+        currentSlideIndex = index;
+        highlightCurrentSlide(currentSlideIndex-1);
+
+    }
+
+    function highlightCurrentSlide(index) {
+        const slideMenuItems = document.querySelector('.carousel-menu').children;
+        [...slideMenuItems].forEach(element => element.classList.remove('current'));
+        slideMenuItems[index].classList.add('current');
+    }
+
+    return {nextSlide, prevSlide, selectSlide}
 })()
