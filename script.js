e098bf2a6c1832bcf314ef83c08b4725580b7fd7
Vinícius Costa <96350207+Vinicius-C13@users.noreply.github.com>
2022-12-22 14:21:26 -0300

    Cart popup and add to cart functionality added

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let currentSlideIndex = 1;
 let slidesGap = 10;
 let slidePosition = 0;
 
+let productsInCart = 0;
+
 function toggleMenu() {
     const nav = document.querySelector('.nav');
     nav.classList.contains('show-nav') ? nav.classList.remove('show-nav') : nav.classList.add('show-nav');
@@ -117,4 +119,54 @@ const carouselPopUp = () => {
     }
 
     return {nextSlide, prevSlide, selectSlide, updateSlide}
+}
+
+function toggleCartMenu() {
+    const cartMenu = document.querySelector('.cart-menu');
+    updateCart();
+    cartMenu.classList.contains('hide') ? cartMenu.classList.remove('hide') : cartMenu.classList.add('hide')
+}
+
+function updateCart() {
+    const cartInfo = document.querySelector('.container-product');
+    if(productsInCart === 0) {
+        cartInfo.innerHTML = `Your cart is empty`
+    } else {
+        cartInfo.innerHTML = `
+            <div class="flex">
+                <div class="cart-image"><img src="./images/image-product-1-thumbnail.jpg"></div>
+                    <div class="info fs-300">
+                    <p>Fall limited edition sneakers</p>
+                    <span class="unit-value">$125,00</span>x<span class="qtt">${productsInCart}</span>
+                    <span class="final-value bold txt-black">$${productsInCart * 125},00</span>
+                </div>
+                <div class="pointer" onclick="cleanCart()"><img src="./images/icon-delete.svg" alt=""></div>
+            </div>
+            <button class="txt-white bold fs-400 pointer">Checkout</button>
+        `
+    }
+}
+
+function cleanCart() {
+    productsInCart = 0;
+    updateCart();
+}
+
+function oneMore() {
+    const counter = document.querySelector(".add-to-cart > div > span");
+    counter.textContent = Number(counter.textContent) + 1;
+}
+
+function oneLess() {
+    const counter = document.querySelector(".add-to-cart > div > span");
+    counter.textContent = Number(counter.textContent) > 0 ? Number(counter.textContent) - 1 : 0;
+}
+
+function addToCart() {
+    let number = Number(document.querySelector(".add-to-cart > div > span").textContent);
+
+    if(number > 0)
+        productsInCart = number;
+    else
+        return 
 }
